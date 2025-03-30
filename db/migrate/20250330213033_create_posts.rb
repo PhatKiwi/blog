702eee67a28f693a25ab267a5745a099fc9f1f98
Ethan Anderson <ethanandy90@gmail.com>
2025-03-30 16:03:59 -0600

    fix migration and add posts controller

--- db/migrate/20250330213033_create_posts.rb
+++ db/migrate/20250330213033_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
       t.string :title
       t.text :body
       t.datetime :published_at
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_30_213033) do
     t.string "title"
     t.text "body"
     t.datetime "published_at"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_30_213033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
