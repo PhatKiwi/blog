aed4eecb1e02d29d54833f0add27edb8b21aa0a5
Ethan Anderson <ethanandy90@gmail.com>
2025-03-31 17:14:54 -0600

    clean up a little bit of the vibe

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment.destroy
-    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
+    redirect_to @post, notice: 'Comment was successfully destroyed.'
   end
 
   private
