37281af12e1fd72f13c56caa3df55242c52c64d8
Ethan Anderson <ethanandy90@gmail.com>
2025-03-31 17:08:28 -0600

    add ability to comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_comment, only: [:destroy]
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @post
+    else
+      flash.now[:danger] = "error"
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  resources :posts
-  resources :comments, only: [:create, :destroy]
+  resources :posts do
+    resources :comments, only: [:create, :new, :destroy]
+  end
 
   # Defines the root path route ("/")
   root "posts#index"
