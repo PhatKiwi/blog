eba828270b5511946f5152d239526baf6559c716
Ethan Anderson <ethanandy90@gmail.com>
2025-04-11 12:32:06 -0600

    add ability to upload avatars to user profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,26 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: [:create, :update]
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  def comment_avatar
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_user_avatar.jpg')),
+      filename: 'default_user_avatar.jpg',
+      content_type: 'image/jpg'
+    )
+  end
 end
